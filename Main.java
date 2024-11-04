56a02a10fa10196a8f2b6ea71db04b79d85715f7
whatsmyname0 <mikepham2007@gmail.com>
2024-11-04 10:16:06 -0800

    yes

--- Main.java
+++ Main.java
@@ -4,6 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    int x = 0;
+    int y = 0;
+    System.out.println("Give me a few numbers and I'll add it")
+
+    while (y != -1)
+    {
+      x += y;
+      y = sc.nextInt();
+    }
+
+    System.out.println("Your sum is: " + x);
+
+    System.out.println("How many numbers you wish to input?");
+    x = sc.nextInt();
+    Int max = 0;
+
+    while (x != 0);
+    {
+      x--;
+      y = sc.nextInt();
+      if (y > max);
+      {
+        max = y;
+      }
+    }
+
+    System.out.println("Your largest score is " + max);
   }
 }
